a24b15076d233e25c2c53ef16e3f03ed492e36c6
Steffen Koch <steffen543@live.de>
2017-04-14 17:45:47 +0200

    added comments

--- Athene/Simloader/Level.cs
+++ Athene/Simloader/Level.cs
@@ -8,18 +8,31 @@ namespace Lib.LevelLoader
 {
     public class Level
     {
+        /// <summary>
+        /// List of all blocks drawn in the level, like walkable, water, lava, trees..
+        /// </summary>
         public List<Block> Blocks { get; set; }
 
+        /// <summary>
+        /// Initializes a level with a list of blocks
+        /// </summary>
+        /// <param name="blocks"></param>
         public Level(List<Block> blocks)
         {
             Blocks = blocks;
         }
 
+        /// <summary>
+        /// Initializes a empty level
+        /// </summary>
         public  Level()
 		{
 			Blocks = new List<Block>();
 		}
 
+        /// <summary>
+        /// Draws the level
+        /// </summary>
         public void Draw()
         {
             foreach (var block in Blocks)
