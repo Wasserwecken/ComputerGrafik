8a5f98f397a903fd6a80d3ce65db808f1dddf624
Steffen Koch <steffen543@live.de>
2017-06-13 13:38:00 +0200

    added Playercommunication interface

---
+++ Athene/Lib.Level/Items/IPlayerCommunication.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+
+namespace Lib.Level.Items
+{
+    public interface IPlayerCommunication
+    {
+        void SetSpawnPosition(Vector2 position);
+        bool IsShooter();
+    }
+}
